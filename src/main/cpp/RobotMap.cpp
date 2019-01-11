986c3492344829ed350ad9684f69be4bfcd1c13c
CRUMCH <unconfigured@null.spigotmc.org>
2019-01-10 20:42:09 -0600

    Set up talon robotmaps

--- src/main/cpp/RobotMap.cpp
+++ src/main/cpp/RobotMap.cpp
@@ -1,7 +1,24 @@
 
 #include <RobotMap.h>
 
+std::shared_ptr<WPI_TalonSRX> RobotMap::Talon1;
+std::shared_ptr<WPI_TalonSRX> RobotMap::Talon2;
+std::shared_ptr<WPI_TalonSRX> RobotMap::Talon3;
+std::shared_ptr<WPI_TalonSRX> RobotMap::Talon4;
+
+std::shared_ptr<WPI_TalonSRX> RobotMap::Mecanums;
+
 void RobotMap::Init()
 {
-    
+    Talon1.reset(new WPI_TalonSRX(1));
+    Talon2.reset(new WPI_TalonSRX(2));
+    Talon3.reset(new WPI_TalonSRX(3));
+    Talon4.reset(new WPI_TalonSRX(4));
+
+    Talon1->SetInverted(true);
+    Talon2->SetInverted(true);
+    Talon3->SetInverted(true);
+    Talon4->SetInverted(true);
+
+    //Talon1->
 }
--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -6,6 +6,15 @@
 class RobotMap 
 {
 public:
+    
+    // One time setup funtion
     static void Init();
     
+    // Drive train initializer
+    static std::shared_ptr<WPI_TalonSRX> Talon1;
+    static std::shared_ptr<WPI_TalonSRX> Talon2;
+    static std::shared_ptr<WPI_TalonSRX> Talon3;
+    static std::shared_ptr<WPI_TalonSRX> Talon4;
+
+    static std::shared_ptr<frc::MecanumDrive> Mecanums;
 };
