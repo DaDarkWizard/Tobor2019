a30125acb54e931d87a399c12dab808cdd28fa90
CRUMCH <unconfigured@null.spigotmc.org>
2019-01-11 19:52:59 -0600

    Set up joysticks

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -5,5 +5,41 @@
 
 OI::OI()
 {
- 
+    Joystick yoke{0};
+    Joystick xbox{1};
+}
+
+double OI::XboxVertL()
+{
+    return (xbox.GetRawAxis(1) * -1);
+}
+
+double OI::XboxHorzL()
+{
+    return xbox.GetRawAxis(0);
+}
+
+double OI::XboxVertR()
+{
+    return (xbox.GetRawAxis(5) * -1);
+}
+
+double OI::XboxHorzR()
+{
+    return xbox.GetRawAxis(4);
+}
+
+double OI::YokeVert()
+{
+    return (yoke.GetRawAxis(1) * -1);
+}
+
+double OI::YokeHorz()
+{
+    return xbox.GetRawAxis(0);
+}
+
+double OI::YokeWheel()
+{
+    return (xbox.GetRawAxis(2) * -1);
 }
--- src/main/include/OI.h
+++ src/main/include/OI.h
@@ -1,14 +1,25 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 #pragma once
 
+#include <frc/WPILib.h>
+
 class OI
 {
- public:
-  OI();
+  private:
+
+    frc::Joystick yoke{0};
+    frc::Joystick xbox{1};
+
+public:
+
+    double XboxVertL();
+    double XboxHorzL();
+    double XboxVertR();
+    double XboxHorzR();
+    
+    double YokeVert();
+    double YokeHorz();
+    double YokeWheel();
+
+    OI();
 };
