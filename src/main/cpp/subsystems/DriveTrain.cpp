fd281e3043b72dd8bb59723bc5dc2de9f91f2d88
CRUMCH <unconfigured@null.spigotmc.org>
2019-01-11 18:24:19 -0600

    Wrote mecanum drive code

--- src/main/cpp/subsystems/DriveTrain.cpp
+++ src/main/cpp/subsystems/DriveTrain.cpp
@@ -1,18 +1,39 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 #include "subsystems/DriveTrain.h"
 
 DriveTrain::DriveTrain() : Subsystem("ExampleSubsystem") {}
 
-void DriveTrain::InitDefaultCommand() {
-  // Set the default command for a subsystem here.
-  // SetDefaultCommand(new MySpecialCommand());
+void DriveTrain::InitDefaultCommand()
+{
+
 }
 
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+void DriveTrain::MecanumDrive(double X, double Y, double Twist)
+{
+  RobotMap::Mecanums->DriveCartesian(X, Y, Twist);
+}
+
+void DriveTrain::StopMecanums()
+{
+  RobotMap::Mecanum->DriveCartesian(0, 0, 0);
+}
+
+void DriveTrain::Talon1(double speed)
+{
+  RobotMap::Talon1->Set(-speed);
+}
+
+void DriveTrain::Talon2(double speed)
+{
+  RobotMap::Talon2->Set(-speed);
+}
+
+void DriveTrain::Talon3(double speed)
+{
+  RobotMap::Talon3->Set(-speed);
+}
+
+void DriveTrain::Talon4(double speed)
+{
+  RobotMap::Talon4->Set(-speed);
+}
