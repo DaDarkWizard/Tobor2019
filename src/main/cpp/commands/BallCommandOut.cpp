6ef92be082b3a74f41840d4ff680e241909fa768
DragonAce5o9 <DragonAce509@gmail.com>
2019-01-15 16:39:00 -0600

    did things

--- src/main/cpp/commands/BallCommandOut.cpp
+++ src/main/cpp/commands/BallCommandOut.cpp
@@ -1,29 +1,38 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "commands/BallCommandOut.h"
 
 BallCommandOut::BallCommandOut() {
   // Use Requires() here to declare subsystem dependencies
   // eg. Requires(Robot::chassis.get());
+  Requires(Robot::ballControl.get());
 }
 
 // Called just before this Command runs the first time
-void BallCommandOut::Initialize() {}
+void BallCommandOut::Initialize() 
+{
+
+}
 
 // Called repeatedly when this Command is scheduled to run
-void BallCommandOut::Execute() {}
+void BallCommandOut::Execute() 
+{
+  Robot::ballControl->
+}
 
 // Make this return true when this Command no longer needs to run execute()
-bool BallCommandOut::IsFinished() { return false; }
+bool BallCommandOut::IsFinished() 
+{
+  return false; 
+}
 
 // Called once after isFinished returns true
-void BallCommandOut::End() {}
+void BallCommandOut::End() 
+{
+
+}
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void BallCommandOut::Interrupted() {}
+void BallCommandOut::Interrupted() 
+{
+
+}
