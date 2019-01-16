4f388dbddf1f9754fa6a4f9195bf3e5444e0c812
CRUMCH <unconfigured@null.spigotmc.org>
2019-01-16 15:49:27 -0600

    Cleaned up new substsytem

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -1,18 +1,12 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 #include "subsystems/Elevator.h"
 
-Elevator::Elevator() : Subsystem("ExampleSubsystem") {}
-
-void Elevator::InitDefaultCommand() {
-  // Set the default command for a subsystem here.
-  // SetDefaultCommand(new MySpecialCommand());
+Elevator::Elevator() : Subsystem("ExampleSubsystem") 
+{
+  
 }
 
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+void Elevator::InitDefaultCommand()
+{
+  // SetDefaultCommand(new MySpecialCommand());
+}
