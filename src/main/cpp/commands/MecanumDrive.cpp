f6614933f6cdbb84595ec0126337ea1b0c50676c
Dasker <danimask@ilc.edu>
2019-01-17 17:35:16 -0600

    put speedwheel back

--- src/main/cpp/commands/MecanumDrive.cpp
+++ src/main/cpp/commands/MecanumDrive.cpp
@@ -16,7 +16,7 @@ void MecanumDrive::Initialize()
 void MecanumDrive::Execute() 
 {
   double speedWheel = (&Robot::m_oi->YokeWheel());
-  frc::speedwheel = (speedwheel * 0.375);
+  speedwheel = (speedwheel * 0.375);
   speedwheel = (speedwheel + 0.625);
   &Robot::driveTrain->MecanumDrive((&Robot::m_oi->XboxHorzR() * speedwheel), 
                                   (&Robot::m_oi->XboxVertR() * speedwheel), 
