a096e1d374513e596e57dd893ea6034f32818623
Dasker <danimask@ilc.edu>
2019-01-17 16:45:52 -0600

    added &

--- src/main/cpp/commands/BallCommandIn.cpp
+++ src/main/cpp/commands/BallCommandIn.cpp
@@ -14,7 +14,7 @@ void BallCommandIn::Initialize()
 
 void BallCommandIn::Execute() 
 {
-  Robot::ballControl->Victor1(0.25);
+  &Robot::ballControl->Victor1(0.25);
 }
 
 bool BallCommandIn::IsFinished() 
