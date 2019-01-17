f9c697bc937261a917e2133d8dd29b785ac4798a
Dasker <danimask@ilc.edu>
2019-01-17 16:55:42 -0600

    fixed more stuff

--- src/main/cpp/RobotMap.cpp
+++ src/main/cpp/RobotMap.cpp
@@ -9,7 +9,7 @@ std::shared_ptr<WPI_TalonSRX> RobotMap::Talon4;
 std::shared_ptr<frc::VictorSP> RobotMap::Victor1;
 std::shared_ptr<frc::VictorSP> RobotMap::Victor2;
 
-std::shared_ptr<frcMecanumDrive> RobotMap::Mecanums;
+std::shared_ptr<frc::MecanumDrive> RobotMap::Mecanums;
 
 void RobotMap::Init()
 {
