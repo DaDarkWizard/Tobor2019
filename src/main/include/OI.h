bf41b093539d8f48e0187c96c0eb934bde6d9332
Dasker <danimask@ilc.edu>
2019-01-18 16:06:35 -0600

    kk

--- src/main/include/OI.h
+++ src/main/include/OI.h
@@ -8,13 +8,13 @@
 class OI
 {
   private:
-
+    /*
     frc::Joystick yoke{0};
     
     frc::Joystick xbox{1};
     
     frc::JoystickButton xboxRB{&xbox, 6};
-    frc::JoystickButton xboxLB{&xbox, 5};
+    frc::JoystickButton xboxLB{&xbox, 5};*/
     
 public:
     OI();
