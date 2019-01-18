079a1a57cb6210ff637b693cdd44c06f9c4a6d99
Dasker <danimask@ilc.edu>
2019-01-18 15:34:31 -0600

    Commented out 'working' code

--- src/main/cpp/commands/MecanumDrive.cpp
+++ src/main/cpp/commands/MecanumDrive.cpp
@@ -15,12 +15,12 @@ void MecanumDrive::Initialize()
 
 void MecanumDrive::Execute() 
 {
-  double speedWheel = (Robot::m_oi->YokeWheel());
+  /*double speedWheel = (Robot::m_oi->YokeWheel());
   speedWheel = (speedWheel * 0.375);
   speedWheel = (speedWheel + 0.625);
   Robot::driveTrain->MecanumDrive((Robot::m_oi->XboxHorzR() * speedWheel), 
                                   (Robot::m_oi->XboxVertR() * speedWheel), 
-                                  (Robot::m_oi->XboxHorzL() * speedWheel));
+                                  (Robot::m_oi->XboxHorzL() * speedWheel));*/
 }
 
 bool MecanumDrive::IsFinished() 
@@ -30,7 +30,7 @@ bool MecanumDrive::IsFinished()
 
 void MecanumDrive::End() 
 {
-  Robot::driveTrain->StopMecanums();
+  //Robot::driveTrain->StopMecanums();
 }
 
 void MecanumDrive::Interrupted() 
