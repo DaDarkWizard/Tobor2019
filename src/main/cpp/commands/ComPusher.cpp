21da81d89788d1f99f356126489992b4eb6bc1c9
DragonAce5o9 <DragonAce509@gmail.com>
2019-01-28 17:39:55 -0600

    made some (possibly incorrect) comments on comPusher.cpp

--- src/main/cpp/commands/ComPusher.cpp
+++ src/main/cpp/commands/ComPusher.cpp
@@ -5,14 +5,17 @@
 
 #include <frc/WPILib.h>
 
+//creates a variable wait time and sets it to 0
 double waitTime = 0;
 bool isDone = false;
 
 ComPusher::ComPusher() : frc::Command("ComPusher")
 {
+  //tells comPusher that it needs the pusher object from the robot class
   Requires(&Robot::pusher);
 }
 
+//a function that runs once when the command is called
 void ComPusher::Initialize() 
 {
   Robot::pusher.Push();
