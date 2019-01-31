089c07639c85aa798249013d2464c044cd23cc42
DragonAce5o9 <DragonAce509@gmail.com>
2019-01-31 17:42:25 -0600

    Commented more stuff

--- src/main/cpp/commands/ComRelease.cpp
+++ src/main/cpp/commands/ComRelease.cpp
@@ -16,22 +16,27 @@ void ComRelease::Initialize()
   Robot::grabber.Release();
 }
 
+//a function that runs
 void ComRelease::Execute() 
 {
 
 }
 
+//tells the robot if it is done
 bool ComRelease::IsFinished() 
 {
   return true; 
 }
 
+//runs when the program ends
 void ComRelease::End() 
 {
 
 }
 
+//runs when the program is interrupted
 void ComRelease::Interrupted() 
 {
+  //runs the thing that runs when the program ends
   End();
 }
